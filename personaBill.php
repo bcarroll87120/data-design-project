1d02acddbbd40de196e8a24a178b46204f8b92c9
Brandon Carroll <bcarroll@live.com>
2019-01-16 15:57:30 -0700

    Started a User Story with Bill Baker.

--- personaBill.php
+++ personaBill.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<h1>Data Design Project</h1>
-		<h3>User Persona</h3>
+		<h2>User Persona</h2>
 		<img src="persona-bill-baker.jpg" alt="Bill">
 			<ul>
 				<li><strong>Name: </strong>Bill Baker</li>
@@ -21,8 +21,13 @@
 						<li>Google Chrome</li>
 					</ul>
 				</li>
-				<li><strong>Personality: </strong>Business-like and professional; generally reserved. All work and no play. Married with two young children.</li>
-				<li><strong>Goals for using website:</strong></li>
+				<li><strong>Personality: </strong>
+					<ul>
+						<li>Business-like and professional</li>
+						<li>Married with two young children</li>
+						<li>Generally reserved; introverted. All work and no play</li>
+						<li><strong>Goals for using website:</strong></li>
+					</ul>
 					<ul>
 						<li>Click on and read news articles</li>
 						<li>To share such articles</li>
@@ -30,13 +35,16 @@
 						<li>To contribute a small monetary donation annualy</li>
 					</ul>
 				<li><strong>User Frustrations: </strong></li>
-					</ul>
+					<ul>
 						<li>Pop-up ads</li>
-		<h3>User Story:</h3>
-		<p>Bill would like to click on and read news articles. He occasionally clicks on
-		journalist profiles to read other material by them. He also occasionally shares
-		articles on social media platforms like Facebook or Twitter.</p>
+						<li>Lack of a share button on mobile to share articles on Reddit</li>
+						<li>The slow decline of humanity</li>
+					</ul>
+		<h2>User Story:</h2>
+			<p>Bill would like to click on and read news articles. He occasionally clicks on
+			journalist profiles to read other material by them. He also occasionally shares
+			articles on social media platforms like Facebook or Twitter.</p>
+		<h2><a href="use-case.php">Use Case</a></h2>
+		<h2><a href="conceptual-model.php">Conceptual Model</a></h2>
 	</body>
-	<h2><a href="use-case.php">Use Case</a></h2>
-	<h2><a href="conceptual-model.php">Conceptual Model</a></h2>
 </html>
