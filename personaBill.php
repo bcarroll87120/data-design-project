a53ab4c4d44bd0bf198cb7ad695dc95501c8689b
Brandon Carroll <bcarroll@live.com>
2019-01-16 13:43:11 -0700

    Started a User Story with Bill Baker.

---
+++ personaBill.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<h3>User Persona</h3>
+		<img src="persona-bill-baker.jpg" alt="Bill">
+		<div>
+			<ul>
+				<li>Name: Bill Baker</li>
+				<li>Age: </li>
+				<li>Gender: </li>
+				<li>Ethnicity: </li>
+				<li>Occupation: Systems analyst</li>
+				<li>Technology: Acer laptop, iPhone 7, Windows 10</li>
+				<li>Attitude: </li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+		<h3>User Story:</h3>
+		<p>Bill would like to click on and read news articles. He occasionally clicks on
+		journalist profiles to read other material by them. He also occasionally shares
+		articles on social media platforms like Facebook or Twitter.</p>
+	</body>
+</html>
