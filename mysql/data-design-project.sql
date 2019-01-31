71a1601c151ef9767d7b08e7b423ab3c23a7177e
Brandon Carroll <bcarroll@live.com>
2019-01-31 10:07:48 -0700

    Started a User Story with Bill Baker.

--- mysql/data-design-project.sql
+++ mysql/data-design-project.sql
@@ -1,48 +1,48 @@
 ALTER DATABASE bcarroll3 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS category;
 
-CREATE TABLE article (
-   articleId BINARY(16) NOT NULL,
-	articleTitle BINARY(16) NOT NULL,
-	articleSubtitle VARCHAR(128) NOT NULL,
-	articleImage VARCHAR(255),
-	articleContent(text) NOT NULL,
-	articleReporter VARCHAR(32) NOT NULL,
+CREATE TABLE category (
+	categoryId BINARY(128) NOT NULL,
+	categorySubtitle VARCHAR(128) NOT NULL,
+	categoryTitle VARCHAR(128) NOT NULL,
 
-   INDEX,
-   PRIMARY KEY (articleId)
+	PRIMARY KEY (categoryId)
 );
 
 CREATE TABLE user (
 	userId BINARY(16) NOT NULL,
-	userName VARCHAR(32) NOT NULL,
-	userLocation VARCHAR(16),
 	userAboutMe VARCHAR(128),
-	userInterests VARCHAR(128),
-	userImage VARCHAR(255),
 	userEmail VARCHAR(32),
+	userImage VARCHAR(255),
+	userInterests VARCHAR(128),
+	userLocation VARCHAR(16),
+	userName VARCHAR(32) NOT NULL,
 
 UNIQUE (userName),
 UNIQUE (userEmail),
 PRIMARY KEY (userId)
 );
 
-CREATE TABLE category (
-	categoryId BINARY(16) NOT NULL,
-	categoryTitle VARCHAR(16) NOT NULL,
-	categorySubtitle VARCHAR(16) NOT NULL,
-
-PRIMARY KEY (categoryId)
+CREATE TABLE article (
+	articleId BINARY(16) NOT NULL,
+	articleCategoryId BINARY(16) NOT NULL,
+	articleContent text NOT NULL,
+	articleImage VARCHAR(255),
+	articleReporter VARCHAR(32) NOT NULL,
+	articleTitle VARCHAR(128) NOT NULL,
+	INDEX (articleCategoryId),
+	PRIMARY KEY (articleId),
+	FOREIGN KEY (articleCategoryId) REFERENCES category(categoryId)
 );
 
 CREATE TABLE comment (
-	commentId BINARY(16) NOT NULL,
-	commentUserId BINARY(16) NOT NULL,
-	commentArticleId BINARY (16) NOT NULL,
+	commentId BINARY(128) NOT NULL,
+	commentArticleId BINARY(128) NOT NULL,
+	commentUserId BINARY(128) NOT NULL,
 	commentContent VARCHAR(128) NOT NULL,
 
 INDEX (commentUserId),
--- mysql/oml.sql
+++ mysql/oml.sql
@@ -1,14 +1,12 @@
-INSERT INTO article(articleContent, articleId, articleImage, articleReporter, articleSubtitle, articleTitle);
-VALUES ("content", UNHEX("ba98a54f7a4e4810a0d40eac98c88f90"), "image", john doe, "this is an article subtitle", "this is an article title");
-INSERT INTO user(userAboutMe, userEmail, userId, userImage, userInterests, userLocation, userName);
-VALUES ("about me", email@email.com, UNHEX("acd927be72174a898e499a5f43367d52"), image, interests, New Mexico, johndoe1);
-INSERT INTO category(categoryId, categoryTitle, categorySubtitle);
-VALUES (UNHEX("6874f3cc3b094a59ba2ec245d6c7197b"), "this is a category title", "this is a category subtitle");
-INSERT INTO comment(commentId, commentUserId, commentArticleId, commentContent);
-VALUES (UNHEX("97041791f0754a4d90adb4ffd77c7081"), UNHEX("acd927be72174a898e499a5f43367d52"), UNHEX("ba98a54f7a4e4810a0d40eac98c88f90"), content;
+INSERT INTO category(categoryId, categoryTitle, categorySubtitle) VALUES (UNHEX("6874f3cc3b094a59ba2ec245d6c7197b"), "this is a category title", "this is a category subtitle");
+INSERT INTO article(articleId, articleCategoryId , articleContent, articleImage, articleReporter, articleTitle) VALUES (UNHEX("ba98a54f7a4e4810a0d40eac98c88f90"), UNHEX("6874f3cc3b094a59ba2ec245d6c7197b"), "content", "image", "john doe", "this is an article title");
+INSERT INTO user(userId, userAboutMe, userEmail, userImage, userInterests, userLocation, userName) VALUES (UNHEX("acd927be72174a898e499a5f43367d52"), "about me", "email@email.com", "image", "interests", "New Mexico", "johndoe1");
+INSERT INTO comment(commentId, commentUserId, commentArticleId, commentContent) VALUES (UNHEX("7560772355114ae589e857f06ada2b93"), '0xACD927BE72174A898E499A5F43367D52', UNHEX("ba98a54f7a4e4810a0d40eac98c88f90"), "content");
 
 UPDATE user SET userAboutMe= UNHEX("acd927be72174a898e499a5f43367d52");
 
 DELETE FROM comment WHERE commentId= UNHEX("97041791f0754a4d90adb4ffd77c7081");
 
-SELECT articleContent, articleId, articleImage, articleReporter, articleSubtitle, articleTitle from article
+SELECT articleContent, articleId, articleImage, articleReporter, articleTitle from article WHERE articleId = UNHEX("ba98a54f7a4e4810a0d40eac98c88f90");
+
+SELECT article.articleContent, article.articleId, article.articleImage, article.articleReporter, article.articleTitle FROM article INNER JOIN category on article.articleId = category.categoryId where article.articleContent= "blah";
